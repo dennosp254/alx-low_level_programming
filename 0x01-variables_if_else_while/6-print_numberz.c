ddadf35cc5154040307423ab617c8d718b9c4419
dennosp254 <dennobasil2@gmail.com>
2023-05-22 23:25:40 +0300

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Print numbers
+  * Return: (0) Success
+  */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
