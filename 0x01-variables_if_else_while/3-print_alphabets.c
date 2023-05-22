41d811d0cc994346971bbd440a95e8cde827d948
dennosp254 <dennobasil2@gmail.com>
2023-05-22 21:01:30 +0300

    alphABETS

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * print alphabet in lower and uppercase
+  * Return: (0) Suuccess
+  */
+int main(void)
+{
+	int i, j;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	for (j = 65; j <= 90; i++)
+	{
+		putchar(j);
+	}
+	putchar('j');
+	return (0);
+}
