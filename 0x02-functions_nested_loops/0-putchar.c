d4928b110386eca28671596900ccb3a82038cc8e
dennosp254 <dennobasil2@gmail.com>
2023-05-28 22:12:50 +0300

    Fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
 #include "main.h"
 /**
-  * main - a program that prints _putchar
-  * Return: Always 0 (Success)
+  *main- a program that prints _putchar, followed by a new line.
+  *Return: Always 0 (Success)
   */
+
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+  * _putchar - writes the character c to stdout
+  * @c: The character to print
+  * Return: On success 1.
+  * On error is set and error is set appropriately
+  */
+int _putchar(char c);
+/**
+  * print_alphabet print lowercase alphabet
+  */
+void print_alphabet(void);
+
+#endif
