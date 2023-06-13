571007161965e165208563b1373474a3a88b605e
dennosp254 <dennobasil2@gmail.com>
2023-06-13 20:33:56 +0300

    100 times table

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,25 +1,38 @@
 #include <stdio.h>
-/**
- * print_to_98 - Prints all natural numbers from n to 98
- * @n: first number to be printed
- */
+
 void print_to_98(int n)
 {
-	int i;
-
 	if (n <= 98)
 	{
-		for (i = n; i < 98; i++)
+		for (int i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			std::cout << i;
+			if (i != 98)
+			{
+				std::cout << ", ";
+			}
 		}
 	}
 	else
 	{
-		for (i = n; i > 98; i--)
+		for (int i = n; i >= 98; i--)
 		{
-			printf("%d, ", i);
+			std::cout << i;
+			if (i != 98)
+			{
+				std::cout << ", ";
+			}
 		}
 	}
-	printf("98\n");
+	std::cout << std::endl;
+}
+
+int main()
+{
+	int number;
+	std::cout << "Enter a number: ";
+	std::cin >> number;
+	print_to_98(number);
+
+	return 0;
 }
