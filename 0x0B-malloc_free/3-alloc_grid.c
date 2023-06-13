36ce92d1233511e0a5c4bacc6a3a8e60b32e087e
dennosp254 <dennobasil2@gmail.com>
2023-06-13 23:53:29 +0300

    alloc grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+
+/**
+ * alloc_grid - initialize elements of a grid to 0
+ * array of integers
+ * @width: First integer
+ * @height: Second integer
+ *
+ * Return: Pointer to a 2 dimensional array of integers,
+ * Null on failure or if the the width or height is 0 or negative
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid, i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = (int **) malloc(height * sizeof(int *));
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *) malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+
+	return (grid);
+}
