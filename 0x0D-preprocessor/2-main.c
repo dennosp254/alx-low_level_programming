8f0328413652ab4d513f5af9ab329e3cc2af0cb7
dennosp254 <dennobasil2@gmail.com>
2023-06-19 19:47:41 +0300

    Main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - prints the name of the file of the program
+  *        was compiled from followed by a newline
+  * Return: Always 0.
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
