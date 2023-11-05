b4a4314015470cc362c24c59a36984881e6340ba
Dennis Ombati <dennobasil2@gmail.com>
2023-11-05 08:49:16 +0300

    fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,11 +4,13 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
+
 /**
-  * read_textfile - reads a txt file and prints it to the POSIX standard output
-  * @filename: name of the file to read
-  * @letters: number of letters it should read and print
-  * Return: number of letters it could read and print
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -16,9 +18,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t lenr, lenw;
 	char *buffer;
 
-	if (file name == NULL)
+	if (filename == NULL)
 		return (0);
-	fd = open(filename 0_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
